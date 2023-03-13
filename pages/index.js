a5c85bee987c8c23079f13006f3b5554e09e6ec5
alaa bourega <93167924+Alaabo@users.noreply.github.com>
2023-03-13 14:24:41 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,15 +8,15 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Toolbox</title>
+        <title>Running New E-Learning Platform</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Next.js Toolbox" />
+        <Header title="This is my test project for mern stack app" />
         <hr />
         <p className="description">
-          Here's an example of a Netlify Form! When you fill this out, the
+          Here'This is a test project for hosting Mern Apps On Netlify
           submissions can be found in the Netlify Admin site.
         </p>
         <FeedbackForm />
